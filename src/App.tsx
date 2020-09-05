ae058a513a77d4fdbb90d8ee866264eb4b5334bc
dosanhoon <hangru1106@naver.com>
2020-09-05 19:36:57 +0900

    기본 기능 init
    api 호출
    카드 렌더링

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+
+import "./App.css";
+import Card, { CardType } from "./components/Card";
 
 function App() {
+  const [list, setList] = React.useState([]);
+  const [page, setPage] = React.useState(1);
+
+  const getCardList = async (page: number) => {
+    try {
+      const API_URL = `https://s3.ap-northeast-2.amazonaws.com/bucketplace-coding-test/cards/page_${page}.json`;
+      const { data } = await axios.get(API_URL);
+      setList(data);
+    } catch (e) {
+      console.log("getCardList error");
+    }
+  };
+
+  React.useEffect(() => {
+    getCardList(page);
+  }, [page]);
+
+  const _renderCardList = React.useMemo(() => {
+    if (list) {
+      return list.map((card: CardType) => <Card key={card.id} card={card} />);
+    }
+    return [];
+  }, [list]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="PC-__">
+      <p>
+        <img
+          src="img/bt-checkbox-checked.svg"
+          className="bt_checkbox_checked"
+          alt="checkbox"
+        />
+        <span className="scrap_tit">스크랩한 것만 보기</span>
+      </p>
+      <section>{_renderCardList}</section>
+    </main>
   );
 }
 
